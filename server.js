dc59a10167d5cd387acc967feb61819d10046c64
Rob Bettigole <rob@elmvc.com>
2015-01-28 04:17:46 +0000

    add nickname route

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ app.get('/', function(request, response){
     response.send('Hello MGT-656!  Really!');
 });
 app.get('/nickname', function(request, response){
-    response.send('Lively Hamster');
+    response.send('lively-hamster');
 });
 app.listen(process.env.PORT || 4000);
